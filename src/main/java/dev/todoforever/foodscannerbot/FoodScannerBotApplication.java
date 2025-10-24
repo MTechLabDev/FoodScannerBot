65bf12a920d8ed6bcb19f1c46e798a976284749f
MTechLab Dev <dliapromua@gmail.com>
2025-10-24 21:47:51 +0300

    init

---
+++ src/main/java/dev/todoforever/foodscannerbot/FoodScannerBotApplication.java
@@ -0,0 +1,13 @@
+package dev.todoforever.foodscannerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodScannerBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodScannerBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/todoforever/foodscannerbot/FoodScannerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.todoforever.foodscannerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodScannerBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
